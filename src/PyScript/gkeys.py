cc56de8a76eb204c56e0ff09c9115d8fca1554c0
ZiftK <alvaro.cab.lag@gmail.com>
2023-10-07 21:53:57 -0700

    g.py

---
+++ src/PyScript/gkeys.py
@@ -0,0 +1,7 @@
+
+
+from api_io.consultor import  Consultor
+
+a = Consultor("api_io\\planets_system.csv")
+
+print(a.get_flat_keys())
---
+++ src/PyScript/grow.py
@@ -0,0 +1,5 @@
+from api_io.consultor import  Consultor
+
+a = Consultor("api_io\\planets_system.csv")
+
+print(a.get_flat_row())
