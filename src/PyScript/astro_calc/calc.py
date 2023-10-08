1fc326069908c6c1acb9ea44350df14edcd7c856
ZiftK <alvaro.cab.lag@gmail.com>
2023-10-08 05:14:02 -0700

    gcalc

--- src/PyScript/astro_calc/calc.py
+++ src/PyScript/astro_calc/calc.py
@@ -2,9 +2,11 @@
 from astropy import units as u
 from astropy import coordinates as coord
 from skyfield.api import load
+from skyfield.errors import EphemerisRangeError
 from datetime import datetime
 import math
 
+# fecha dd/mm/aa, p1 ,p2, cohete
 # Mapeo de nombres a identificadores de Skyfield
 planet_mapping = {
     'Mercury': 'mercury',
@@ -24,6 +26,7 @@ rocket_speeds = {
     "Falcon Heavy": 7.5,
     "Delta IV Heavy": 9.2
 }
+
 
 def get_planet_positions(date_str=None):
     # Cargar efemérides
@@ -70,3 +73,42 @@ def calculate_distance(planet_data, planet1, planet2):
 
 def calculate_travel_time(distance, speed):
     return distance * 1.496e8 / speed  # Convertir UA a km y dividir por la velocidad para obtener el tiempo en segundos
+
+
+def calc(p1,p2,rocket,date = None):
+    # Solicitar entrada del usuario
+    user_input = date
+
+    planet1 = p1
+    planet2 = p2
+
+    selected_rocket = rocket
+
+    try:
+        if user_input:
+            day, month, year = map(int, user_input.split('/'))
+            planet_data = get_planet_positions(f"{year}-{month}-{day}")
+        else:
+            planet_data = get_planet_positions()
+
+    except EphemerisRangeError:
+        print("La fecha ingresada está fuera del rango cubierto por el archivo de efemérides. Por favor, elige una fecha entre 1899-07-29 y 2053-10-09.")
+        planet_data = None
+
+    # Asegúrate de que solo continuamos si planet_data es válido
+    if planet_data:
+
+       
+
+        if selected_rocket not in rocket_speeds:
+            print("Cohete no reconocido.")
+            exit()
+
+        distance = calculate_distance(planet_data, planet1, planet2)
+        time_seconds = calculate_travel_time(distance, rocket_speeds[selected_rocket])
+
+        hours = time_seconds // 3600
+        minutes = (time_seconds % 3600) // 60
+        seconds = time_seconds % 60
+
+        print(f"El tiempo de viaje entre {planet1} y {planet2} usando el {selected_rocket} es aproximadamente {int(hours)} horas, {int(minutes)} minutos y {seconds:.2f} segundos.")
