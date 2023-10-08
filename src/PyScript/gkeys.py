6162390aa8ddb9717a064b7d30be876100df6cf6
ZiftK <alvaro.cab.lag@gmail.com>
2023-10-08 13:11:55 -0700

    pre pull

--- src/PyScript/gkeys.py
+++ src/PyScript/gkeys.py
@@ -1,15 +1,24 @@
-from api_io.consultor import  Consultor
+# Import the 'Consultor' class from the 'api_io.consultor' module and the 'sys' module for handling command-line arguments.
+from plugin_io.consultor import Consultor
 import sys
 
-columnas = sys.argv[1]
-a = Consultor("api_io\\planets_system.csv")
+# Retrieve the column names from the command-line arguments.
+column_names = sys.argv[1]
 
-cl = columnas.split(",")
-print(cl)
+# Create an instance of the 'Consultor' class and provide the path to a CSV file containing data (e.g., "planets_system.csv").
+consultor_instance = Consultor("plugin_io\\planets_system.csv")
 
+# Split the provided column names into a list.
+column_list = column_names.split(",")
+
+# Print the list of column names.
+print(column_list)
+
+# Try to retrieve and print the flattened keys for the specified columns using the 'get_flat_keys' method.
 try:
-    print(a.get_flat_keys(*cl))
+    flattened_keys = consultor_instance.get_flat_keys(*column_list)
+    print(flattened_keys)
 
+# Catch a KeyError if the specified columns are not found.
 except KeyError:
-    print("No se hallaron registros")
-
+    print("No records found for the specified columns.")
--- src/PyScript/grow.py
+++ src/PyScript/grow.py
@@ -1,11 +1,18 @@
-from api_io.consultor import  Consultor
+# Import the 'Consultor' class from the 'api_io.consultor' module and the 'sys' module for handling command-line arguments.
+from plugin_io.consultor import Consultor
 import sys
 
-planeta = sys.argv[1]
+# Retrieve the name of the planet from the command-line arguments.
+planet_name = sys.argv[1]
 
-a = Consultor("api_io\\planets_system.csv")
+# Create an instance of the 'Consultor' class and provide the path to a CSV file containing data (e.g., "planets_system.csv").
+consultor_instance = Consultor("plugin_io\\planets_system.csv")
 
+# Try to retrieve and print the flattened row data for the specified planet using the 'get_flat_row' method.
 try:
-    print(a.get_flat_row(planeta))
+    flattened_row_data = consultor_instance.get_flat_row(planet_name)
+    print(flattened_row_data)
+
+# Catch a KeyError if the specified planet name is not found in the data.
 except KeyError:
-    print("No se hallaron registros")
+    print("No records found for the specified planet.")
