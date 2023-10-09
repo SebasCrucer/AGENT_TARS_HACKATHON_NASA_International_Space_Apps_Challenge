c33a75e50ffdd10e693ef5a65783e4c270391a44
ZiftK <alvaro.cab.lag@gmail.com>
2023-10-08 21:30:20 -0700

    add calcs docs

--- src/PyScript/grow.py
+++ src/PyScript/grow.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Import the 'Consultor' class from the 'api_io.consultor' module and the 'sys' module for handling command-line arguments.
 from plugin_io.consultor import Consultor
 import sys
@@ -17,20 +16,3 @@ try:
 # Catch a KeyError if the specified planet name is not found in the data.
 except KeyError:
     print("No records found for the specified planet.")
-=======
-# Importación de módulos y bibliotecas necesarios
-from api_io.consultor import Consultor  # Importa la clase Consultor del módulo consultor dentro del paquete api_io
-import sys  # Biblioteca estándar para interactuar con el intérprete de Python
-
-# Obtiene el argumento pasado al script (nombre del planeta) desde la línea de comandos
-planeta = sys.argv[1]
-
-# Crea una instancia de la clase Consultor usando el archivo 'planets_system.csv' localizado en el subdirectorio 'api_io'
-a = Consultor("api_io\\planets_system.csv")
-
-# Intenta recuperar y mostrar la fila correspondiente al planeta especificado desde el archivo CSV
-try:
-    print(a.get_flat_row(planeta))
-except KeyError:  # Si el planeta no se encuentra en el archivo, se lanza una excepción KeyError
-    print("No se hallaron registros")
->>>>>>> d5e791f5114dd33e57b75c4eb106d61330bc1bda
